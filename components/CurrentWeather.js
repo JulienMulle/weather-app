4ad686ab150c4ebb08994a1805ee87ca6ef20888
Julien <julienmulle@gmail.com>
2021-10-08 11:47:34 +0200

    useState pour afficher la prevision souhaiter

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,10 +1,20 @@
+import { isSameDay } from "date-fns";
 import React, { useEffect } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function CurrentWeather( {data}) {
+    const [currentWeather, setcurrentWeather] = useState(null)
 
     useEffect(() => {
-        data.list.filter(forecast)
+        const currentW = data.list.filter(forecast => {
+            //prevision du jour
+            const today = new Date().getTime()+ Math.abs(data.city.timezone * 1000)
+            // prevision des jours à venir
+            const forecastDate = new Date(forecast.dt *1000)
+            return isSameDay( today, forecastDate)
+        })
+        //j'utilise useState pour afficher la prevision souhaiter
+        setcurrentWeather(currentW[0])
     }, [data])
 
     return (
