3204ed4ec47290cc41b79c7e2823e2bb64b17032
Julien <julienmulle@gmail.com>
2021-10-08 11:42:30 +0200

    logique pour les prevision du jour + à venir

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 
 export default function CurrentWeather( {data}) {
+
+    useEffect(() => {
+        data.list.filter(forecast)
+    }, [data])
 
     return (
         <>
